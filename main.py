b90dc0fdbac23c0029028160a7079a9179ebfd99
Amira Hassan <90963846+AmiraHassan2003@users.noreply.github.com>
2025-02-13 03:51:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,24 +25,24 @@ returnDateList = borrow.getReturnDate()
 
 
     ######################## Book ##########################
-# print(book.addBook("title1", "author1", "genre1"))
+# print(book.addBook("The Great Gatsby", "F. Scott Fitzgerald", "Fiction"))
 # print(book.removeBook(12))
 
-# print(book.searchTitleBook("www", titles, authors, genres))
-# print(book.searchAuthorBook("a1", titles, authors, genres))
-# print(book.searchGenreBook("333", titles, authors, genres))
+# print(book.searchTitleBook("To Kill a Mockingbird", titles, authors, genres))
+# print(book.searchAuthorBook("J.K. Rowling", titles, authors, genres))
+# print(book.searchGenreBook("Mystery", titles, authors, genres))
 
-# print(book.updateTitle(7, "qqqqq", ids_book, authors, genres ))
-# print(book.updateAuthor(5, "sss", ids_book, titles, genres))
-# print(book.updateGenre(5, "gggg", ids_book, titles, authors))
+# print(book.updateTitle(7, "1984", ids_book, authors, genres))
+# print(book.updateAuthor(5, "George Orwell", ids_book, titles, genres))
+# print(book.updateGenre(5, "Dystopian", ids_book, titles, authors))
 
 # print(book.getAllBooks(titles, authors, genres))
 
         #################### Member ############################
-# print(member.addMember("mmmm", "012345"))
+# print(member.addMember("Alice Johnson", "0123456789"))
 # print(member.viewMember(2, names, phones))
-# print(member.updateName(3, "nenen", ids_member, phones))
-# print(member.updatePhone(3, "11111", ids_member, names))
+# print(member.updateName(3, "Michael Smith", ids_member, phones))
+# print(member.updatePhone(3, "0987654321", ids_member, names))
 
         ######################## Borrowing #######################
 # print(borrow.borrowBook(2, 10, "6/7/20003", "6/8/2003", booksList, memberBorrowingList))
